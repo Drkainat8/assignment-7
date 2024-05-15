af704d5b2c6bc479b0e0f0b16c1c164931dfc25b
Drkainat8 <161299000+Drkainat8@users.noreply.github.com>
2024-05-15 23:39:57 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+//assignment 8 //result of additin subtraction multiplication devision should be 8
+var additionresult = 4 + 4;
+console.log("Additionresult", additionresult);
+var subtractionresult = 10 - 2;
+console.log("Subtractionreult", subtractionresult);
+var multiplicationresult = 2 * 4;
+console.log("Multiplicationresult", multiplicationresult);
+var divisonresult = 16 / 2;
+console.log("Divisionresult", divisonresult);
---
+++ main.ts
@@ -0,0 +1,12 @@
+//assignment 7 //result of additin subtraction multiplication devision should be 8
+let additionresult : number = 4 + 4;
+console.log("Additionresult" , additionresult)
+
+let subtractionresult : number = 10 -2;
+console.log("Subtractionreult" , subtractionresult)
+
+let multiplicationresult : number = 2 * 4;
+console.log("Multiplicationresult" , multiplicationresult)
+
+let divisonresult : number = 16 / 2;
+console.log("Divisionresult" , divisonresult)
